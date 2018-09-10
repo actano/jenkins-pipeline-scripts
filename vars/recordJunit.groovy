e9bb72eae3cc9562c23cd39f9c09d2d56bbe0ed1
Marcus Mennemeier <marcus.mennemeier@actano.de>
2018-09-10 18:07:46 +0200

    fixup! Add wrapper around junit call to pass back actual test result count

--- vars/recordJunit.groovy
+++ vars/recordJunit.groovy
@@ -1,17 +1,30 @@
 import hudson.tasks.test.AbstractTestResultAction
 
 Map<String,Integer> call(Map props) {
-    AbstractTestResultAction testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-    int total = testResultAction == null ? 0 : testResultAction.getTotalCount()
-    int failed = testResultAction == null ? 0 : testResultAction.getFailCount()
-    int skipped = testResultAction == null ? 0 : testResultAction.getSkipCount()
+    int total = 0
+    int failed = 0
+    int skipped = 0
+    try {
+        AbstractTestResultAction testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+        if (testResultAction != null) {
+            total = testResultAction.getTotalCount()
+            failed = testResultAction.getFailCount()
+            skipped = testResultAction.getSkipCount()
+        }
+    } catch (ignored) {
+    }
 
     junit(props)
 
-    testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-    total = (testResultAction == null ? 0 : testResultAction.getTotalCount()) - total
-    failed = (testResultAction == null ? 0 : testResultAction.getFailCount()) - failed
-    skipped = (testResultAction == null ? 0 : testResultAction.getSkipCount()) - skipped
+    try {
+        AbstractTestResultAction testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+        if (testResultAction != null) {
+            total = testResultAction.getTotalCount() - total
+            failed = testResultAction.getFailCount() - failed
+            skipped = testResultAction.getSkipCount() - skipped
+        }
+    } catch (ignored) {
+    }
     return [
         total: total,
         failed: failed,
